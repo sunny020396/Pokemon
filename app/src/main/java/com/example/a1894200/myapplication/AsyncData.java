3aa34d7e6f5eebb1fbf93f5f262401ad81185390
[akhilreddymedapati] <[akhilreddymedapati]>
2019-07-11 16:45:00 -0400

    App Name addedgff

---
+++ app/src/main/java/com/example/a1894200/myapplication/AsyncData.java
@@ -0,0 +1,9 @@
+package com.example.a1894200.myapplication;
+
+import android.os.AsyncTask;
+
+public class AsyncData extends AsyncTask<String,Void,String> {
+
+
+
+}
---
+++ app/src/main/java/com/example/a1894200/myapplication/Httphandler.java
@@ -0,0 +1,10 @@
+package com.example.a1894200.myapplication;
+
+public class Httphandler {
+    public Httphandler() {
+
+
+    }
+
+
+}
