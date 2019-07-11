364fc7bf3f2660bf8c26d36e0d0dfc8b249dea85
Jasti <1894200@cegepgim.lcl>
2019-07-11 14:25:08 -0400

    Initial commit

---
+++ app/src/main/java/com/example/a1894200/myapplication/PoKeMoN.java
@@ -0,0 +1,13 @@
+package com.example.a1894200.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PoKeMoN extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_po_ke_mo_n);
+    }
+}
